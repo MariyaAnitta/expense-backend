417e1f728758bb6d914851fe252aed15c55a3dff
Anitta Mariya <mariyaanitta31@gmail.com>
2026-01-23 09:29:53 +0530

    change

--- convert_receipt_token.py
+++ convert_receipt_token.py
@@ -1,17 +1,15 @@
 import pickle
 import base64
 
-# Read the token file
+# Personal Gmail Token
+with open('credentials/token.pickle', 'rb') as f:
+    token1 = base64.b64encode(f.read()).decode('utf-8')
+
+# Receipts Gmail Token  
 with open('credentials/token_receipts.pickle', 'rb') as f:
-    token_data = f.read()
+    token2 = base64.b64encode(f.read()).decode('utf-8')
 
-# Convert to base64
-token_base64 = base64.b64encode(token_data).decode('utf-8')
-
-# Print it
-print("\n" + "="*70)
-print("GMAIL_RECEIPTS_TOKEN_BASE64")
-print("="*70)
-print(token_base64)
-print("="*70)
-print("\nCopy this value and save it - you'll add it to Render next")
+print("=== GMAIL_TOKEN_BASE64 (Personal) ===")
+print(token1)
+print("\n=== GMAIL_RECEIPTS_TOKEN_BASE64 (Receipts) ===")
+print(token2)
